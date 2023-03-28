49757c8769c717521ae8b41177dafb18ae5d0f9c
reinerkp <kate.reiner1@gmail.com>
2023-03-27 18:01:22 -0600

    fixed values in seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,14 +15,14 @@ VALUES (1, "Sales Manager", 1200000),
       (2, "Marketing Specialist", 85000);
 
 INSERT INTO employee (role_id, first_name, last_name, manager_id)
-VALUES (1, "Jacob", "Grell", null),
-        (2, "Joey", "Grund", null),
-        (3, "Josh", "Harm", null),
-        (4, "Evan", "Battlefield", null),
-        (5, "Frodo", "Baggins", 1),
-        (5, "Sam", "Gamgee", 1),
-        (6, "Bruce", "Wayne", 3),
-        (8, "Ash", "Ketchum", 2),
-        (7, "Salazar", "Slytherin", 4),
-        (7, "Edgar", "Poe", 4);
+VALUES (1, "Katherine", "Scott", null),
+        (2, "Christine", "Bushlack", null),
+        (3, "Brennen", "Smith", null),
+        (4, "Evan", "Bardish", null),
+        (5, "Toby", "Kirbel", 1),
+        (5, "Darby", "Willish", 1),
+        (6, "Kathleen", "Wolfram", 3),
+        (8, "Ashley", "Superchi", 2),
+        (7, "Brittany", "Car", 4),
+        (7, "Roland", "Doe", 4);
         
